2a80cfaa7e2834020674f94165f0a8745da76ad3
Earle F. Philhower, III <earlephilhower@yahoo.com>
2021-03-20 11:33:38 -0700

    Fix build path

--- pico-sdk-lib/build.sh
+++ pico-sdk-lib/build.sh
@@ -2,7 +2,7 @@
 rm -rf build
 mkdir build
 cd build
-PICO_SDK_PATH=../../pico-sdk/ PATH="$(cd ../system/arm*/bin; pwd):$PATH" cmake ..
+PICO_SDK_PATH=../../pico-sdk/ PATH="$(cd ../../system/arm-none-eabi/bin; pwd):$PATH" cmake ..
 make
 cp libpico.a ../../lib/.
 
