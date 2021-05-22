4f1386c2639a1cc8795eb653a7217949bf7ade25
Earle F. Philhower, III <earlephilhower@yahoo.com>
2021-05-22 10:32:50 -0700

    Add TinyUSB tests to CI

--- tests/build.sh
+++ tests/build.sh
@@ -16,7 +16,17 @@ elif [ "$BUILD_PARITY" = "odd" ]; then
 fi
 
 install_arduino nodebug
+# Replace the skip function to only build TinyUSB tests
+function skip_ino()
+{
+    local ino=$1
+    echo $ino | grep -q Adafruit_TinyUSB_Arduino
+    echo $(( $? ))
+}
 build_sketches_with_arduino "$mod" "$rem"
+build_sketches_with_arduino "$mod" "$rem"
+
+
 
 rm -rf "$cache_dir"
 
