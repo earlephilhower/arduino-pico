03e740a511775ae762ed226b00edc9d599ba54e0
Earle F. Philhower, III <earlephilhower@yahoo.com>
2021-06-28 20:53:40 -0700

    Add CPU cycle counter accessors (#226)
    Use the 24-bit SYSTICK peripheral, wrapped in logic to extend it to a
    full 32 or 64bits.  W/o the wrapper, SYSTICK will wrap around in ~100ms.
    
    Adds rp2040.getCycleCount() and rp2040.getCycleCount64()
    
    Clean up the libpico build process as crt0.S from the pico-sdk should
    be directly used.
    
    Clean up the keywords file.

---
+++ libraries/rp2040/examples/GetCycleCount/GetCycleCount.ino
@@ -0,0 +1,21 @@
+void setup() {
+  Serial.begin(115200);
+  delay(3000);
+  Serial.println("High precision timing measurements/delays can be built using");
+  Serial.println("rp2040.getCycleCount() and rp2040.getCycleCount64().\n");
+  uint32_t a = rp2040.getCycleCount();
+  delay(1000);
+  uint32_t b = rp2040.getCycleCount();
+  Serial.printf("There are %d cycles in one second.\n\n\n", b - a);
+
+  delay(3000);
+
+  Serial.println("Note that the (32-bit) getCycleCount() will wraparound in ~30 seconds.");
+  Serial.println("Using the 64-bit getCycleCount64() makes it practically infinite.");
+  Serial.printf("\n%15s - %15s\n", "getCycleCount", "getCycleCount64");
+}
+
+void loop() {
+  Serial.printf("%15u - %15llu\n", rp2040.getCycleCount(), rp2040.getCycleCount64());
+  delay(1500);
+}
