881bcd81ba4e6739cbeed1d1b88504be4042f501
Earle F. Philhower, III <earlephilhower@yahoo.com>
2021-12-11 11:53:49 -0800

    Update to SDK version 1.3.0

--- tools/libpico/make-libpico.sh
+++ tools/libpico/make-libpico.sh
@@ -23,6 +23,7 @@ for type in boot2_generic_03h boot2_is25lp080 boot2_w25q080 boot2_w25x10cl; do
         arm-none-eabi-gcc -march=armv6-m -mcpu=cortex-m0plus -mthumb -O3 \
                          -DNDEBUG -DPICO_FLASH_SPI_CLKDIV=$div \
                          -c "$PICO_SDK_PATH/src/rp2_common/boot_stage2/$type.S" \
+                         -I "$PICO_SDK_PATH/src/boards/include/boards/" \
                          -I "$PICO_SDK_PATH/src/rp2040/hardware_regs/include/" \
                          -I "$PICO_SDK_PATH/src/rp2_common/pico_platform/include/" \
                          -I "$PICO_SDK_PATH/src/rp2_common/boot_stage2/asminclude/" \
