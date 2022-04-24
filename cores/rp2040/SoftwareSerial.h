264dfdd35de5533e96738c4697e55f29586717fa
Earle F. Philhower, III <earlephilhower@yahoo.com>
2022-04-24 14:56:12 -0700

    Add SoftwareSerial wrapper around SerialPIO
    I receive mails weekly asking how to use `SoftwareSerial` on this core.
    Avoid the issue by including a simple wrapper class around `SerialPIO`
    which gives the proper class name and constructor parameters.
    
    Note that inverted mode is not supported.

---
+++ cores/rp2040/SoftwareSerial.h
@@ -0,0 +1,35 @@
+/*
+    SoftwareSerial wrapper for SerialPIO
+
+    Copyright (c) 2022 Earle F. Philhower, III <earlephilhower@yahoo.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#pragma once
+
+#include "SerialPIO.h"
+
+class SoftwareSerial : public SerialPIO {
+public:
+    // Note the rx/tx pins are swapped in PIO vs SWSerial
+    SoftwareSerial(pin_size_t rx, pin_size_t tx, bool invert = true) : SerialPIO(tx, rx) {
+        if (invert) {
+            panic("SoftwareSerial inverted operation not supported\n");
+        }
+    }
+    void listen() { /* noop */ }
+    bool isListening() { return true; }
+};
