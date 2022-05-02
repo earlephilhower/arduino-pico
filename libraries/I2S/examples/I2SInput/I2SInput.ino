b547a235c3b7fb8ce6a22c08588609321adda321
Earle F. Philhower, III <earlephilhower@yahoo.com>
2022-05-02 12:22:50 -0700

    Update I2SInput example

--- libraries/I2S/examples/I2SInput/I2SInput.ino
+++ libraries/I2S/examples/I2SInput/I2SInput.ino
@@ -1,6 +1,6 @@
 /*
    I2S stereo microphone (input) example
-   Run using the Arduion Serial Plotter to see waveform.
+   Run using the Arduino Serial Plotter to see waveform.
    Released to the Public Domain by Earle F. Philhower, III
 
    For the Google AIY Voice Hat Microphone daughterboard, part
@@ -10,43 +10,27 @@
             +--   ------------------------------------  --+
    left RPI | (1) GND (2) DIN (3) BCLK (4) LRCLK (5) 3.3V | AIY right
             +---------------------------------------------+
-
-   The I2S pins are on different pins depending on your board.
-   The *internal GPIO number* which is NOT NECESSARIALY the
-   same as the pin numbers, are as follows:
-   I2SI_DATA     = GPIO12
-   IS2I_BCK      = GPIO13
-   I2SI_WS/LRCLK = GPIO14
-
-   On the D1 mini the I2SI pins map to the following D pins:
-   I2SI_DATA     = GPIO12 = D6
-   IS2I_BCK      = GPIO13 = D7
-   I2SI_WS/LRCLK = GPIO14 = D5
-
-   Expect different D pins on different ESP8266 boards, and of
-   course be sure to wire up VCC(3.3V) and GND.
 */
 
-#include <ESP8266WiFi.h>
 #include <I2S.h>
+
+I2S i2s(INPUT);
 
 void setup() {
   Serial.begin(115200);
-  WiFi.forceSleepBegin();
   delay(500);
-
-  i2s_rxtx_begin(true, false);  // Enable I2S RX
-  i2s_set_rate(11025);
+  i2s.setDATA(0);
+  i2s.setBCLK(1); // WCLK = GP2
+  i2s.setBitsPerSample(32);
+  i2s.setFrequency(11025);
+  i2s.begin();
 
   delay(1000);
 
   while (1) {
-    int16_t l, r;
-    i2s_read_sample(&l, &r, true);
-    char withScale[256];
-    sprintf(withScale, "%d %d", l, r);
-    Serial.println(withScale);
-    yield();
+    int32_t l, r;
+    i2s.read32((uint32_t*)&l, (uint32_t*)&r);
+    Serial.printf("%d %d\n", l, r);
   }
 }
 
