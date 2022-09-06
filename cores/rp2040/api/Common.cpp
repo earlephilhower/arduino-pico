4284b351435633ba8210d1f9d8eff4006e32e395
Earle F. Philhower, III <earlephilhower@yahoo.com>
2022-09-06 13:07:08 -0700

    Remove duplicated ArduinoCore-API files
    Use #include .... to reference them from the cores directory to ensure
    they keep up to date.

--- cores/rp2040/api/Common.cpp
+++ cores/rp2040/api/Common.cpp
@@ -1,10 +1,1 @@
-#include "Common.h"
-
-/* C++ prototypes */
-long map(long x, long in_min, long in_max, long out_min, long out_max)
-{
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-uint16_t makeWord(uint16_t w) { return w; }
-uint16_t makeWord(uint8_t h, uint8_t l) { return (h << 8) | l; }
+#include "../../../ArduinoCore-API/api/Common.cpp"
