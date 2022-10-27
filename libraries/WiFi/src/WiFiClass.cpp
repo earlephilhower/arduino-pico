e05a3b8ad0e3bb63589a042462da400ce594b547
mecparts <2958269+mecparts@users.noreply.github.com>
2022-10-27 09:00:59 -0600

    Add a way to return the RSSI value (#934)
    See https://forums.raspberrypi.com/viewtopic.php?t=341774

--- libraries/WiFi/src/WiFiClass.cpp
+++ libraries/WiFi/src/WiFiClass.cpp
@@ -323,8 +323,13 @@ uint8_t* WiFiClass::BSSID(uint8_t* bssid) {
     return: signed value
 */
 int32_t WiFiClass::RSSI() {
-    // TODO - driver does not return this?!
-    return 0;
+#ifndef CYW43_IOCTL_GET_RSSI
+#define CYW43_IOCTL_GET_RSSI 0xFE
+#endif
+
+    int32_t rssi;
+    cyw43_ioctl(&cyw43_state, CYW43_IOCTL_GET_RSSI, sizeof rssi, (uint8_t *)&rssi, CYW43_ITF_STA);
+    return rssi;
 }
 
 /*
