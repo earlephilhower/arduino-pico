d1842743241a6a1294d2b1b26743c6ec693ae400
Earle F. Philhower, III <earlephilhower@yahoo.com>
2022-12-18 14:43:36 -0800

    Fix CI errors when more than one *.a in cache dir (#1049)
    Avoids errors shown in logs for certain builds.

--- tests/common.sh
+++ tests/common.sh
@@ -80,7 +80,8 @@ function build_sketches()
             continue  # Not ours to do
         fi
 
-        if [ -e $cache_dir/core/*.a ]; then
+        cacheas=( $cache_dir/core/*.a )
+        if [ -e ${cacheas[0]} ]; then
             # We need to preserve the build.options.json file and replace the last .ino
             # with this sketch's ino file, or builder will throw everything away.
             jq '."sketchLocation" = "'$sketch'"' $build_dir/build.options.json > $build_dir/build.options.json.tmp
