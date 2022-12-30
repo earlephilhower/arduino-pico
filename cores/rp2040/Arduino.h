91a8a07b1a544b6e20b958128c02d31815c10fc6
neilger <neilger@users.noreply.github.com>
2022-12-30 09:26:30 -0500

    digitalRead/WriteFast to sio_hw->gpio

--- cores/rp2040/Arduino.h
+++ cores/rp2040/Arduino.h
@@ -68,8 +68,8 @@ void noInterrupts();
 #define portOutputRegister(port)    ((volatile uint32_t*) sio_hw->gpio_out)
 #define portInputRegister(port)     ((volatile uint32_t*) sio_hw->gpio_in)
 #define portModeRegister(port)      ((volatile uint32_t*) sio_hw->gpio_oe)
-#define digitalWriteFast(pin, val)  (val ? gpio_set_mask (1 << pin) : gpio_clr_mask(1 << pin))
-#define digitalReadFast(pin)        gpio_get(pin)
+#define digitalWriteFast(pin, val)  (val ? sio_hw->gpio_set = (1 << pin) : sio_hw->gpio_clr = (1 << pin))
+#define digitalReadFast(pin)        ((1 << pin) & sio_hw->gpio_in)
 
 // ADC RP2040-specific calls
 void analogReadResolution(int bits);
