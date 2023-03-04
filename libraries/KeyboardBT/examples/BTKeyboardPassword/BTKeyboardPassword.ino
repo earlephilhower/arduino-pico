c16990998c3e4f674e765c9d5868cd4027fa3a6a
Earle F. Philhower, III <earlephilhower@yahoo.com>
2023-03-04 14:09:57 -0800

    Add Keyboard LED callback for USB and BT (not BLE)
    BLE seems to require some kind of characteristic callback that is not yeet
    implemented here.  USB and BT tested and examples updated.

--- libraries/KeyboardBT/examples/BTKeyboardPassword/BTKeyboardPassword.ino
+++ libraries/KeyboardBT/examples/BTKeyboardPassword/BTKeyboardPassword.ino
@@ -3,9 +3,22 @@
 
 #include <KeyboardBT.h>
 
+void ledCB(bool numlock, bool capslock, bool scrolllock, bool compose, bool kana, void *cbData) {
+  (void) numlock;
+  (void) scrolllock;
+  (void) compose;
+  (void) kana;
+  (void) cbData;
+  digitalWrite(LED_BUILTIN, capslock ? HIGH : LOW);
+}
+
 void setup() {
   Serial.begin(115200);
   KeyboardBT.begin("PicoW Password");
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  KeyboardBT.onLED(ledCB);
+  KeyboardBT.begin();
   delay(5000);
   Serial.printf("Arduino USB Password Typer\n");
   Serial.printf("Press BOOTSEL to enter your super-secure(not!) password\n\n");
