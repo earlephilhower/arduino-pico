9e272733cf9e83cf011fcd63131c1b7d32d7510d
Tim Boldt <timboldt@gmail.com>
2023-03-05 14:24:31 -0800

    Add instructions for making I2S input work with Adafruit microphone (#1272)
    Co-authored-by: timboldt <tim.boldt@gmail.com>

--- libraries/I2S/examples/I2SInput/I2SInput.ino
+++ libraries/I2S/examples/I2SInput/I2SInput.ino
@@ -10,6 +10,19 @@
             +--   ------------------------------------  --+
    left RPI | (1) GND (2) DIN (3) BCLK (4) LRCLK (5) 3.3V | AIY right
        logo +---------------------------------------------+ logo
+
+   For an Adfruit I2S MEMS microphone (https://www.adafruit.com/product/3421),
+   connect the pins as follows:
+
+      DOUT -> GPIO0
+      BCLK <- GPIO1
+      LRCL <- GPIO2  # LRCLK = BCLK + 1
+      GND <-> GND
+      3V  <-> 3V3OUT
+
+   The other idiosyncrasy of most modern MEMS microphones is that they
+   require a minimum clock rate to wake up. For example, the SPH0645
+   microphone needs at least 1MHz.
 */
 
 #include <I2S.h>
@@ -20,15 +33,21 @@ void setup() {
   Serial.begin(115200);
 
   i2s.setDATA(0);
-  i2s.setBCLK(1); // LRCLK = GP2
+  i2s.setBCLK(1); // Note: LRCLK = BCLK + 1
   i2s.setBitsPerSample(16);
   i2s.setFrequency(22050);
+  // NOTE: The following values are known to work with the Adafruit microphone:
+  // i2s.setBitsPerSample(32);
+  // i2s.setFrequency(16000);
   i2s.begin();
 
   while (1) {
     int16_t l, r;
     i2s.read16(&l, &r);
-    Serial.printf("%d %d\n", l, r);
+    // NOTE: Adafruit microphone word size needs to match the BPS above.
+    // int32_t l, r;
+    // i2s.read32(&l, &r);
+    Serial.printf("%d %d\r\n", l, r);
   }
 }
 
