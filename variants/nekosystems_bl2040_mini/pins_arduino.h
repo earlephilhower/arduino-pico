39e9f9dd0d4649f1af865bc98feb7f959a41af8d
nanoparticle <mcnanoparticle@gmail.com>
2023-03-08 20:50:25 -0800

    figured out the pin assignment mess

--- variants/nekosystems_bl2040_mini/pins_arduino.h
+++ variants/nekosystems_bl2040_mini/pins_arduino.h
@@ -32,38 +32,35 @@
 
 
 // STANDARD SECTION:
-// LEDs
+// LED
 #define PIN_LED        (6u)
 
-// Unfortunately all of the below definitions are
-// necessary for the core to compile, but many
-// are superseded by the functions above and should
-// not be used without changing the default pins.
-
 // Serial
-#define PIN_SERIAL1_TX (0u)
-#define PIN_SERIAL1_RX (1u)
+#define PIN_SERIAL1_TX (12u)
+#define PIN_SERIAL1_RX (13u)
 
-#define PIN_SERIAL2_TX (8u)
-#define PIN_SERIAL2_RX (9u)
+#define PIN_SERIAL2_TX (20u)
+#define PIN_SERIAL2_RX (21u)
 
+// NOTE: SPI0 is used by the on-board magnetic angle sensor,
+// do not change the pins assigned to this SPI object!
 // SPI
 #define PIN_SPI0_MISO  (16u)
 #define PIN_SPI0_MOSI  (19u)
 #define PIN_SPI0_SCK   (18u)
 #define PIN_SPI0_SS    (17u)
 
-#define PIN_SPI1_MISO  (12u)
-#define PIN_SPI1_MOSI  (15u)
-#define PIN_SPI1_SCK   (14u)
-#define PIN_SPI1_SS    (13u)
+#define PIN_SPI1_MISO  (8u)
+#define PIN_SPI1_MOSI  (11u)
+#define PIN_SPI1_SCK   (10u)
+#define PIN_SPI1_SS    (9u)
 
 // Wire
-#define PIN_WIRE0_SDA  (4u)
-#define PIN_WIRE0_SCL  (5u)
+#define PIN_WIRE0_SDA  (24u)
+#define PIN_WIRE0_SCL  (25u)
 
-#define PIN_WIRE1_SDA  (26u)
-#define PIN_WIRE1_SCL  (27u)
+#define PIN_WIRE1_SDA  (22u)
+#define PIN_WIRE1_SCL  (23u)
 
 #define SERIAL_HOWMANY (3u)
 #define SPI_HOWMANY    (2u)
