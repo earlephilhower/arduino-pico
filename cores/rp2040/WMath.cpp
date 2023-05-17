b57ac66815ccf5f42be20deb621044b1912e762c
Earle F. Philhower, III <earlephilhower@yahoo.com>
2023-05-16 23:47:17 -0700

    Cleanup legacy includes (#1452)

--- cores/rp2040/WMath.cpp
+++ cores/rp2040/WMath.cpp
@@ -16,8 +16,8 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#include "stdlib.h"
-#include "stdint.h"
+#include <stdlib.h>
+#include <stdint.h>
 
 void randomSeed(uint32_t dwSeed) {
     if (dwSeed != 0) {
