d8c7ff3c74b6e0cb6bddf6c80985cf93b31c1e18
marklinmax <mgraschevalier@icloud.com>
2023-08-02 15:47:32 +0200

    Add SerialUSB::ignoreFlowControl() method

--- cores/rp2040/SerialUSB.cpp
+++ cores/rp2040/SerialUSB.cpp
@@ -130,7 +130,7 @@ size_t SerialUSB::write(const uint8_t *buf, size_t length) {
 
     static uint64_t last_avail_time;
     int written = 0;
-    if (tud_cdc_connected()) {
+    if (tud_cdc_connected() || _ignoreFlowControl) {
         for (size_t i = 0; i < length;) {
             int n = length - i;
             int avail = tud_cdc_write_available();
@@ -171,6 +171,9 @@ SerialUSB::operator bool() {
     return tud_cdc_connected();
 }
 
+void SerialUSB::ignoreFlowControl(bool ignore){
+    _ignoreFlowControl = ignore;
+}
 
 static bool _dtr = false;
 static bool _rts = false;
