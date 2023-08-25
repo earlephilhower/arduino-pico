ade74986ee9f8d9dc8d1d456c601d5cbdbf2e2da
Earle F. Philhower, III <earlephilhower@yahoo.com>
2023-08-25 09:56:52 -0700

    GCC 12.3, GDB 12, OpenOCD 0.12, Picotool 1.1.2 (#1670)
    Major toolchain update including:
    * GCC 12.3
    * GDB 12
    * OpenOCD 0.12
    * Picotool 1.1.2
    
    * Fix MDNS infinite recursion
    
    * Remove legacy Picoprobe
    
    Fixes #1313
    Fixes #1650

--- tools/makeboards.py
+++ tools/makeboards.py
@@ -119,7 +119,6 @@ def BuildIPBTStack(name):
 def BuildUploadMethodMenu(name):
     for a, b, c, d, e, f in [ ["default", "Default (UF2)", 256, "picoprobe_cmsis_dap.tcl", "uf2conv", "uf2conv-network"],
                               ["picotool", "Picotool", 256, "picoprobe.tcl", "picotool", None],
-                              ["picoprobe", "Picoprobe", 256, "picoprobe.tcl", "picoprobe", None],
                               ["picoprobe_cmsis_dap", "Picoprobe (CMSIS-DAP)", 256, "picoprobe_cmsis_dap.tcl", "picoprobe_cmsis_dap", None],
                               ["picodebug", "Pico-Debug", 240, "picodebug.tcl", "picodebug", None] ]:
         print("%s.menu.uploadmethod.%s=%s" % (name, a, b))
