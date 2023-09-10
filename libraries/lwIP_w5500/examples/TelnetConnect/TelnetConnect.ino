29c60ce1c907bb992cfaf4a7218dd193f431dece
Earle F. Philhower, III <earlephilhower@yahoo.com>
2023-09-09 17:10:05 -0700

    Async context to pump LWIP and non-CYW43 ethernet

--- libraries/lwIP_w5500/examples/TelnetConnect/TelnetConnect.ino
+++ libraries/lwIP_w5500/examples/TelnetConnect/TelnetConnect.ino
@@ -2,12 +2,83 @@
 #include <W5500lwIP.h>
 #include <WiFi.h>
 
+#include <pico/async_context_threadsafe_background.h>
+#include <pico/lwip_nosys.h>
+
+//#include <pico/async_context.h>
+
 Wiznet5500lwIP eth(/*SS*/ 1);  // <== adapt to your hardware
 
 const char* host = "djxmmx.net";
 const uint16_t port = 17;
 
+
+static async_context_threadsafe_background_t lwip_ethernet_async_context_threadsafe_background;
+
+async_context_t *lwip_ethernet_init_default_async_context(void) {
+  async_context_threadsafe_background_config_t config = async_context_threadsafe_background_default_config();
+  if (async_context_threadsafe_background_init(&lwip_ethernet_async_context_threadsafe_background, &config)) {
+    return &lwip_ethernet_async_context_threadsafe_background.core;
+  }
+  return NULL;
+}
+
+
+static void update_next_timeout(async_context_t *context, async_when_pending_worker_t *worker);
+static void ethernet_timeout_reached(async_context_t *context, async_at_time_worker_t *worker);
+
+
+static async_when_pending_worker_t always_pending_update_timeout_worker = {
+  .next = 0,
+  .do_work = update_next_timeout,
+  .work_pending = 0,
+  .user_data = 0,
+};
+
+static async_at_time_worker_t ethernet_timeout_worker = {
+  .next = 0,
+  .do_work = ethernet_timeout_reached,
+  .next_time = 0,
+  .user_data = 0,
+};
+
+bool go = false;
+
+static void ethernet_timeout_reached(__unused async_context_t *context, __unused async_at_time_worker_t *worker) {
+  assert(worker == &ethernet_timeout_worker);
+  if (go) {
+    eth.handlePackets(); sys_check_timeouts();
+  }
+}
+
+
+static void update_next_timeout(async_context_t *context, async_when_pending_worker_t *worker) {
+  assert(worker == &always_pending_update_timeout_worker);
+  // we want to run on every execution of the helper to re-reflect any changes
+  // to the underlying lwIP timers which may have happened in the interim
+  // (note that worker will be called on every outermost exit of the async_context
+  // lock, and lwIP timers should not be modified whilst not holding the lock.
+  worker->work_pending = true;
+  uint32_t sleep_ms = sys_timeouts_sleeptime();
+  if (sleep_ms == SYS_TIMEOUTS_SLEEPTIME_INFINITE) {
+    ethernet_timeout_worker.next_time = at_the_end_of_time;
+  } else {
+    ethernet_timeout_worker.next_time = make_timeout_time_ms(sleep_ms);
+  }
+  //            async_context_add_at_time_worker_in_ms(context, &sleep_timeout_worker, CYW43_SLEEP_CHECK_MS);
+  async_context_add_at_time_worker_in_ms(context, &ethernet_timeout_worker, 50);
+}
+
+
+
 void setup() {
+  async_context_t *context  = lwip_ethernet_init_default_async_context();
+
+  //lwip_nosys_init(context);
+  always_pending_update_timeout_worker.work_pending = true;
+  async_context_add_when_pending_worker(context, &always_pending_update_timeout_worker);
+
+
   SPI.setSCK(2);
   SPI.setTX(3);
   SPI.setRX(0);
@@ -42,7 +113,7 @@ void setup() {
     Serial.printf(".");
     delay(1000);
   }
-
+  go = true;
   Serial.printf("Ethernet: IP Address: %s\n",
                 eth.localIP().toString().c_str());
 }
