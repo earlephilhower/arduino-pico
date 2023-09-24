dfd66536505c7f51de9097c977732663ced85d04
maxgerhardt <maximilian.gerhardt@rub.de>
2023-09-24 17:54:19 +0200

    Mostly equialize config with tusb_config_rp2040.h, doesnt seem to have been use actually

--- include/arduino_pico_tusb_config.h
+++ include/arduino_pico_tusb_config.h
@@ -1,95 +1,137 @@
 /*
- * The MIT License (MIT)
- *
- * Copyright (c) 2019 Ha Thach (tinyusb.org)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- */
+    The MIT License (MIT)
 
-#ifndef _TUSB_CONFIG_H_
-#define _TUSB_CONFIG_H_
+    Copyright (c) 2018, hathach for Adafruit
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to
+   deal in the Software without restriction, including without limitation the
+   rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+   sell copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+   IN THE SOFTWARE.
+*/
+
+#ifndef _TUSB_CONFIG_RP2040_H_
+#define _TUSB_CONFIG_RP2040_H_
 
 #ifdef __cplusplus
- extern "C" {
+extern "C" {
 #endif
 
 //--------------------------------------------------------------------
 // COMMON CONFIGURATION
 //--------------------------------------------------------------------
+#define CFG_TUSB_RHPORT0_MODE OPT_MODE_DEVICE
+
+// Enable device stack
+#define CFG_TUD_ENABLED 1
+
+// Enable host stack with pio-usb if Pico-PIO-USB library is available
+#if __has_include("pio_usb.h")
+#define CFG_TUH_ENABLED 1
+#define CFG_TUH_RPI_PIO_USB 1
+#endif
 
 #ifndef CFG_TUSB_MCU
- #define CFG_TUSB_MCU             OPT_MCU_RP2040
+#define CFG_TUSB_MCU OPT_MCU_RP2040
 #endif
+#define CFG_TUSB_OS OPT_OS_PICO
 
-#define CFG_TUSB_RHPORT0_MODE     OPT_MODE_DEVICE
-#define CFG_TUSB_OS               OPT_OS_PICO
-
-// CFG_TUSB_DEBUG is defined by compiler in DEBUG build
 #ifndef CFG_TUSB_DEBUG
-#define CFG_TUSB_DEBUG           0
+#define CFG_TUSB_DEBUG 0
 #endif
 
-/* USB DMA on some MCUs can only access a specific SRAM region with restriction on alignment.
- * Tinyusb use follows macros to declare transferring memory so that they can be put
- * into those specific section.
- * e.g
- * - CFG_TUSB_MEM SECTION : __attribute__ (( section(".usb_ram") ))
- * - CFG_TUSB_MEM_ALIGN   : __attribute__ ((aligned(4)))
- */
-#ifndef CFG_TUSB_MEM_SECTION
 #define CFG_TUSB_MEM_SECTION
-#endif
-
-#ifndef CFG_TUSB_MEM_ALIGN
-#define CFG_TUSB_MEM_ALIGN          __attribute__ ((aligned(4)))
-#endif
+#define CFG_TUSB_MEM_ALIGN TU_ATTR_ALIGNED(4)
 
 //--------------------------------------------------------------------
-// DEVICE CONFIGURATION
+// Device Configuration
 //--------------------------------------------------------------------
 
-#ifndef CFG_TUD_ENDPOINT0_SIZE
-#define CFG_TUD_ENDPOINT0_SIZE    64
-#endif
+#define CFG_TUD_ENDOINT0_SIZE 64
 
-//------------- CLASS -------------//
-#define CFG_TUD_HID              (2)
-#define CFG_TUD_CDC              (1)
-#define CFG_TUD_MSC              (1)
-#define CFG_TUD_MIDI             (0)
-#define CFG_TUD_VENDOR           (0)
+#define CFG_TUD_CDC 1
+#define CFG_TUD_MSC 1
+#define CFG_TUD_HID 2
+#define CFG_TUD_MIDI 1
+#define CFG_TUD_VENDOR 1
 
-#define CFG_TUD_CDC_RX_BUFSIZE  (256)
-#define CFG_TUD_CDC_TX_BUFSIZE  (256)
+// CDC FIFO size of TX and RX
+#define CFG_TUD_CDC_RX_BUFSIZE 256
+#define CFG_TUD_CDC_TX_BUFSIZE 256
 
 // MSC Buffer size of Device Mass storage
 // Was 512 byte for most standard systems, but for internally allocated FatFS
 // We'd rather take the native flash erase size of 4096 bytes.
 #define CFG_TUD_MSC_EP_BUFSIZE (4096) //512
 
-
 // HID buffer size Should be sufficient to hold ID (if any) + Data
-#define CFG_TUD_HID_EP_BUFSIZE  (64)
+#define CFG_TUD_HID_EP_BUFSIZE 64
+
+// MIDI FIFO size of TX and RX
+#define CFG_TUD_MIDI_RX_BUFSIZE 128
+#define CFG_TUD_MIDI_TX_BUFSIZE 128
+
+// Vendor FIFO size of TX and RX
+#define CFG_TUD_VENDOR_RX_BUFSIZE 64
+#define CFG_TUD_VENDOR_TX_BUFSIZE 64
+
+//--------------------------------------------------------------------
+// Host Configuration
+//--------------------------------------------------------------------
+
+// Size of buffer to hold descriptors and other data used for enumeration
+#define CFG_TUH_ENUMERATION_BUFSIZE 256
+
+// Number of hub devices
+#define CFG_TUH_HUB 1
+
+// max device support (excluding hub device): 1 hub typically has 4 ports
+#define CFG_TUH_DEVICE_MAX (3 * CFG_TUH_HUB + 1)
+
+// Enable tuh_edpt_xfer() API
+// #define CFG_TUH_API_EDPT_XFER       1
+
+// Number of mass storage
+#define CFG_TUH_MSC 1
+
+// Number of HIDs
+// typical keyboard + mouse device can have 3,4 HID interfaces
+#define CFG_TUH_HID (3 * CFG_TUH_DEVICE_MAX)
+
+// Number of CDC interfaces
+// FTDI and CP210x are not part of CDC class, only to re-use CDC driver API
+#define CFG_TUH_CDC 1
+#define CFG_TUH_CDC_FTDI 1
+#define CFG_TUH_CDC_CP210X 1
+
+// RX & TX fifo size
+#define CFG_TUH_CDC_RX_BUFSIZE 128
+#define CFG_TUH_CDC_TX_BUFSIZE 128
+
+// Set Line Control state on enumeration/mounted:
+// DTR ( bit 0), RTS (bit 1)
+#define CFG_TUH_CDC_LINE_CONTROL_ON_ENUM 0x03
+
+// Set Line Coding on enumeration/mounted, value for cdc_line_coding_t
+// bit rate = 115200, 1 stop bit, no parity, 8 bit data width
+// This need Pico-PIO-USB at least 0.5.1
+#define CFG_TUH_CDC_LINE_CODING_ON_ENUM                                        \
+  { 115200, CDC_LINE_CONDING_STOP_BITS_1, CDC_LINE_CODING_PARITY_NONE, 8 }
 
 #ifdef __cplusplus
- }
+}
 #endif
 
-#endif /* _TUSB_CONFIG_H_ */
+#endif /* _TUSB_CONFIG_RP2040_H_ */
