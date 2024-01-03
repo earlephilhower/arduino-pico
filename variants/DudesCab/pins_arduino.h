7d06f029e5fe8b36d13d7ef64d7f1966e4a8064a
Arnoz <arnoz@arnoz.com>
2024-01-03 15:37:27 +0100

    Update pins_arduino.h

--- variants/DudesCab/pins_arduino.h
+++ variants/DudesCab/pins_arduino.h
@@ -8,11 +8,8 @@
 #define PIN_LED        (25u)
 
 // Serial
-#define PIN_SERIAL1_TX (0u)
-#define PIN_SERIAL1_RX (1u)
-
-#define PIN_SERIAL2_TX (8u)
-#define PIN_SERIAL2_RX (9u)
+#define PIN_SERIAL1_TX (8u)
+#define PIN_SERIAL1_RX (9u)
 
 // SPI
 #define PIN_SPI0_MISO  (4u)
@@ -20,20 +17,12 @@
 #define PIN_SPI0_SCK   (2u)
 #define PIN_SPI0_SS    (5u)
 
-#define PIN_SPI1_MISO  (12u)
-#define PIN_SPI1_MOSI  (15u)
-#define PIN_SPI1_SCK   (14u)
-#define PIN_SPI1_SS    (13u)
-
 // Wire
-#define PIN_WIRE0_SDA  (18u)
-#define PIN_WIRE0_SCL  (19u)
+#define PIN_WIRE0_SDA  (0u)
+#define PIN_WIRE0_SCL  (1u)
 
-#define PIN_WIRE1_SDA  (14u)
-#define PIN_WIRE1_SCL  (15u)
-
-#define SERIAL_HOWMANY (3u)
-#define SPI_HOWMANY    (2u)
-#define WIRE_HOWMANY   (2u)
+#define SERIAL_HOWMANY (1u)
+#define SPI_HOWMANY    (1u)
+#define WIRE_HOWMANY   (1u)
 
 #include "../generic/common.h"
