454e7fbc531c0ca7a499c2c2f4f0cb973f65893d
arnoz26 <arnoz@arnoz.com>
2024-01-10 17:41:47 +0100

    Update pins_arduino.h

--- variants/DudesCab/pins_arduino.h
+++ variants/DudesCab/pins_arduino.h
@@ -8,10 +8,11 @@
 #define PIN_LED        (25u)
 
 // Serial
-#define PIN_SERIAL1_TX (8u)
-#define PIN_SERIAL1_RX (9u)
-#define PIN_SERIAL2_TX (0u)
-#define PIN_SERIAL2_RX (1u)
+#define PIN_SERIAL1_TX (0u)
+#define PIN_SERIAL1_RX (1u)
+
+#define PIN_SERIAL2_TX (8u)
+#define PIN_SERIAL2_RX (9u)
 
 // SPI
 #define PIN_SPI0_MISO  (4u)
@@ -19,14 +20,20 @@
 #define PIN_SPI0_SCK   (2u)
 #define PIN_SPI0_SS    (5u)
 
-// Wire
-#define PIN_WIRE0_SDA  (0u)
-#define PIN_WIRE0_SCL  (1u)
-#define PIN_WIRE1_SDA  (0u)
-#define PIN_WIRE1_SCL  (1u)
+#define PIN_SPI1_MISO  (12u)
+#define PIN_SPI1_MOSI  (15u)
+#define PIN_SPI1_SCK   (14u)
+#define PIN_SPI1_SS    (13u)
 
-#define SERIAL_HOWMANY (2u)
-#define SPI_HOWMANY    (1u)
+// Wire
+#define PIN_WIRE0_SDA  (18u)
+#define PIN_WIRE0_SCL  (19u)
+
+#define PIN_WIRE1_SDA  (10u)
+#define PIN_WIRE1_SCL  (11u)
+
+#define SERIAL_HOWMANY (3u)
+#define SPI_HOWMANY    (2u)
 #define WIRE_HOWMANY   (2u)
 
 #include "../generic/common.h"
