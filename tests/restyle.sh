cd84d5ae639cc584b3cbed66d13161130fb9f85d
Earle F. Philhower, III <earlephilhower@yahoo.com>
2024-02-11 11:29:35 -0800

    Fix warning in lwip_ESPHost, add to styler

--- tests/restyle.sh
+++ tests/restyle.sh
@@ -14,7 +14,7 @@ for dir in ./cores/rp2040 ./libraries/EEPROM ./libraries/I2S ./libraries/SingleF
            ./libraries/MouseBT ./libraries/SerialBT ./libraries/HID_Bluetooth \
            ./libraries/JoystickBLE ./libraries/KeyboardBLE ./libraries/MouseBLE \
            ./libraries/lwIP_w5500 ./libraries/lwIP_w5100 ./libraries/lwIP_enc28j60 \
-           ./libraries/SPISlave ; do
+           ./libraries/SPISlave ./libraries/lwIP_ESPHost; do
     find $dir -type f \( -name "*.c" -o -name "*.h" -o -name "*.cpp" \) -a  \! -path '*api*' -exec astyle --suffix=none --options=./tests/astyle_core.conf \{\} \;
     find $dir -type f -name "*.ino" -exec astyle --suffix=none --options=./tests/astyle_examples.conf \{\} \;
 done
