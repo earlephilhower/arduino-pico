03b68621697f8a7fc1c42705efb8a700263a4866
Earle F. Philhower, III <earlephilhower@yahoo.com>
2024-06-04 13:56:37 -0700

    Bluetooth Master HID and musical keyboard example
    Adds BluetoothHIDMaster and HIDKeyStream which let the PicoW connect to
    and use Bluetooth Classic HID devices like keyboards and mice.
    
    An example that lets the PicoW use a BT keyboard as a piano is
    included and shows the use of the new classes.

--- tests/restyle.sh
+++ tests/restyle.sh
@@ -15,7 +15,8 @@ for dir in ./cores/rp2040 ./libraries/EEPROM ./libraries/I2S ./libraries/SingleF
            ./libraries/JoystickBLE ./libraries/KeyboardBLE ./libraries/MouseBLE \
            ./libraries/lwIP_w5500 ./libraries/lwIP_w5100 ./libraries/lwIP_enc28j60 \
            ./libraries/SPISlave ./libraries/lwIP_ESPHost ./libraries/FatFS\
-           ./libraries/FatFSUSB ./libraries/BluetoothAudio ./libraries/BluetoothHCI; do
+           ./libraries/FatFSUSB ./libraries/BluetoothAudio ./libraries/BluetoothHCI \
+           ./libraries/BluetoothHIDMaster; do
     find $dir -type f \( -name "*.c" -o -name "*.h" -o -name "*.cpp" \) -a  \! -path '*api*' -exec astyle --suffix=none --options=./tests/astyle_core.conf \{\} \;
     find $dir -type f -name "*.ino" -exec astyle --suffix=none --options=./tests/astyle_examples.conf \{\} \;
 done
