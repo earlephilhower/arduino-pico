a584d10321e28ecdae0396701db4a66efb61c290
AcThPaU <31486033+AcThPaU@users.noreply.github.com>
2024-07-13 05:41:06 +0800

    Fix D pin mapping on Adafruit IB/Feather RP2040 (#2259)
    * Fix D pins on Adafruit IB RP2040
    * Fix Adafruit Feather D pins mapping as well
    * Change common.h to accept D pin def per board

--- variants/adafruit_feather/pins_arduino.h
+++ variants/adafruit_feather/pins_arduino.h
@@ -41,4 +41,9 @@
 #define SPI_HOWMANY    (1u)
 #define WIRE_HOWMANY   (1u)
 
+// D pins
+#define __PIN_D4             (6u)
+#define __PIN_D5             (7u)
+#define __PIN_D6             (8u)
+
 #include "../generic/common.h"
