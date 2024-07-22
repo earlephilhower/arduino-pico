ce8383b5d4fff768514fe084ade7bf7c1bb83563
Earle F. Philhower, III <earlephilhower@yahoo.com>
2024-07-22 12:28:46 -0700

    Split variant build, combine spell + style checks
    Variant builds were taking longer than the actual individual CI jobs, so
    split it up.
    
    Compile the spelling and style checks, they ran very fast and spent more
    time in checking out than in running.

--- tests/restyle.sh
+++ tests/restyle.sh
@@ -2,7 +2,7 @@
 
 for dir in ./cores/rp2040 ./libraries/EEPROM ./libraries/I2S ./libraries/SingleFileDrive \
            ./libraries/LittleFS/src ./libraries/LittleFS/examples ./libraries/PWMAudio \
-           ./libraries/rp2040 ./libraries/SD ./libraries/ESP8266SdFat ./libraries/ADCInput \
+           ./libraries/rp2040 ./libraries/SD ./libraries/ADCInput \
            ./libraries/Servo ./libraries/SPI ./libraries/Wire ./libraries/PDM \
            ./libraries/WiFi ./libraries/lwIP_Ethernet ./libraries/lwIP_CYW43 \
            ./libraries/FreeRTOS/src ./libraries/LEAmDNS ./libraries/MD5Builder \
