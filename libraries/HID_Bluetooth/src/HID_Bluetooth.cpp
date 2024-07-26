8da30ec446681265ba64bfc4056263ae45581832
Earle F. Philhower, III <earlephilhower@yahoo.com>
2024-07-26 08:54:07 -0700

    Factor out cut-n-pasted GAMEPAD16 HID structures
    Create a single spot with the gamepad16's HID report descriptor and
    report structure.  Avoids cut-n-pasted code.

--- libraries/HID_Bluetooth/src/HID_Bluetooth.cpp
+++ libraries/HID_Bluetooth/src/HID_Bluetooth.cpp
@@ -1,46 +1,5 @@
 #include "HID_Bluetooth.h"
-
-#define TUD_HID_REPORT_DESC_GAMEPAD16(...) \
-  HID_USAGE_PAGE ( HID_USAGE_PAGE_DESKTOP     )                 ,\
-  HID_USAGE      ( HID_USAGE_DESKTOP_GAMEPAD  )                 ,\
-  HID_COLLECTION ( HID_COLLECTION_APPLICATION )                 ,\
-    /* Report ID if any */\
-    __VA_ARGS__ \
-    /* 16 bit X, Y, Z, Rz, Rx, Ry (min -32767, max 32767 ) */ \
-    HID_USAGE_PAGE     ( HID_USAGE_PAGE_DESKTOP                 ) ,\
-    HID_USAGE          ( HID_USAGE_DESKTOP_X                    ) ,\
-    HID_USAGE          ( HID_USAGE_DESKTOP_Y                    ) ,\
-    HID_USAGE          ( HID_USAGE_DESKTOP_Z                    ) ,\
-    HID_USAGE          ( HID_USAGE_DESKTOP_RZ                   ) ,\
-    HID_USAGE          ( HID_USAGE_DESKTOP_RX                   ) ,\
-    HID_USAGE          ( HID_USAGE_DESKTOP_RY                   ) ,\
-    HID_LOGICAL_MIN_N  ( -32767, 2                              ) ,\
-    HID_LOGICAL_MAX_N  ( 32767, 2                               ) ,\
-    HID_REPORT_COUNT   ( 6                                      ) ,\
-    HID_REPORT_SIZE    ( 16                                     ) ,\
-    HID_INPUT          ( HID_DATA | HID_VARIABLE | HID_ABSOLUTE ) ,\
-    /* 8 bit DPad/Hat Button Map  */ \
-    HID_USAGE_PAGE     ( HID_USAGE_PAGE_DESKTOP                 ) ,\
-    HID_USAGE          ( HID_USAGE_DESKTOP_HAT_SWITCH           ) ,\
-    HID_LOGICAL_MIN    ( 1                                      ) ,\
-    HID_LOGICAL_MAX    ( 8                                      ) ,\
-    HID_PHYSICAL_MIN   ( 0                                      ) ,\
-    HID_PHYSICAL_MAX_N ( 315, 2                                 ) ,\
-    HID_REPORT_COUNT   ( 1                                      ) ,\
-    HID_REPORT_SIZE    ( 8                                      ) ,\
-    HID_INPUT          ( HID_DATA | HID_VARIABLE | HID_ABSOLUTE ) ,\
-    /* 32 bit Button Map */ \
-    HID_USAGE_PAGE     ( HID_USAGE_PAGE_BUTTON                  ) ,\
-    HID_USAGE_MIN      ( 1                                      ) ,\
-    HID_USAGE_MAX      ( 32                                     ) ,\
-    HID_LOGICAL_MIN    ( 0                                      ) ,\
-    HID_LOGICAL_MAX    ( 1                                      ) ,\
-    HID_REPORT_COUNT   ( 32                                     ) ,\
-    HID_REPORT_SIZE    ( 1                                      ) ,\
-    HID_INPUT          ( HID_DATA | HID_VARIABLE | HID_ABSOLUTE ) ,\
-  HID_COLLECTION_END \
-
-
+#include <sdkoverride/tusb_gamepad16.h>
 
 //setup the report map.
 //more generic function to be used with BLE & BT Classis
