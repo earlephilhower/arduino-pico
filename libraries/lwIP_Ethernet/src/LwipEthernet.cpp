a89805a445c5beb936ae731526a964e09f297a97
Earle F. Philhower, III <earlephilhower@yahoo.com>
2024-08-16 19:41:19 -0700

    Fix Ethernet GPIO

--- libraries/lwIP_Ethernet/src/LwipEthernet.cpp
+++ libraries/lwIP_Ethernet/src/LwipEthernet.cpp
@@ -79,7 +79,7 @@ static uint32_t gpioMask[4] = {0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff};
 void ethernet_arch_lwip_gpio_mask() {
     noInterrupts();
     memmove(gpioMaskStack[1], gpioMaskStack[0], 4 * sizeof(uint32_t) * (GPIOSTACKSIZE - 1)); // Push down the stack
-    io_irq_ctrl_hw_t *irq_ctrl_base = get_core_num() ? &iobank0_hw->proc1_irq_ctrl : &iobank0_hw->proc0_irq_ctrl;
+    io_bank0_irq_ctrl_hw_t *irq_ctrl_base = get_core_num() ? &io_bank0_hw->proc1_irq_ctrl : &io_bank0_hw->proc0_irq_ctrl;
     for (int i = 0; i < 4; i++) {
         gpioMaskStack[0][i] = irq_ctrl_base->inte[i];
         irq_ctrl_base->inte[i] = irq_ctrl_base->inte[i] & gpioMask[i];
@@ -89,7 +89,7 @@ void ethernet_arch_lwip_gpio_mask() {
 
 void ethernet_arch_lwip_gpio_unmask() {
     noInterrupts();
-    io_irq_ctrl_hw_t *irq_ctrl_base = get_core_num() ? &iobank0_hw->proc1_irq_ctrl : &iobank0_hw->proc0_irq_ctrl;
+    io_bank0_irq_ctrl_hw_t *irq_ctrl_base = get_core_num() ? &io_bank0_hw->proc1_irq_ctrl : &io_bank0_hw->proc0_irq_ctrl;
     for (int i = 0; i < 4; i++) {
         irq_ctrl_base->inte[i] = gpioMaskStack[0][i];
     }
