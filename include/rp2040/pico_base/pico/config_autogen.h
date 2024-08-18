4e0c6792df9434be56cba48b13d85f2638d392e8
Earle F. Philhower, III <earlephilhower@yahoo.com>
2024-08-18 11:04:01 -0700

    Relative paths for config_autogen.h

--- include/rp2040/pico_base/pico/config_autogen.h
+++ include/rp2040/pico_base/pico/config_autogen.h
@@ -4,7 +4,7 @@
 
 // based on PICO_CONFIG_HEADER_FILES:
 
-#include "/home/earle/Arduino/hardware/pico/rp2040/pico-sdk/src/boards/include/boards/pico_w.h"
-#include "/home/earle/Arduino/hardware/pico/rp2040/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h"
+#include "../../pico-sdk/src/boards/include/boards/pico_w.h"
+#include "../../pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h"
 
 // based on PICO_RP2040_CONFIG_HEADER_FILES:
