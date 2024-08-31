00eda8f5d6e28ba15c6b65ef544cee7b7b6e5067
Earle F. Philhower, III <earlephilhower@yahoo.com>
2024-08-30 17:23:59 -0700

    Add serial inversion for UART and SerialPIO
    Use real GPIO pad inversion to allow inverted RX, TX, and controls for
    the hardware UART and software PIO-emulated serial ports.
    
    Adds ``setInvertTX(bool)`` and ``setInvertRX(bool)`` calls to both ports,
    with ``setInvertControl(bool)`` for the HW UARTS.

--- cores/rp2040/pio_uart.pio.h
+++ cores/rp2040/pio_uart.pio.h
@@ -44,44 +44,6 @@ static inline pio_sm_config pio_tx_program_get_default_config(uint offset) {
     sm_config_set_sideset(&c, 2, true, false);
     return c;
 }
-#endif
-
-// ---------- //
-// pio_tx_inv //
-// ---------- //
-
-#define pio_tx_inv_wrap_target 0
-#define pio_tx_inv_wrap 5
-#define pio_tx_inv_pio_version 0
-
-static const uint16_t pio_tx_inv_program_instructions[] = {
-    //     .wrap_target
-    0xe029, //  0: set    x, 9
-    0x90a0, //  1: pull   block           side 0
-    0x6001, //  2: out    pins, 1
-    0xa046, //  3: mov    y, isr
-    0x0084, //  4: jmp    y--, 4
-    0x0042, //  5: jmp    x--, 2
-    //     .wrap
-};
-
-#if !PICO_NO_HARDWARE
-static const struct pio_program pio_tx_inv_program = {
-    .instructions = pio_tx_inv_program_instructions,
-    .length = 6,
-    .origin = -1,
-    .pio_version = 0,
-#if PICO_PIO_VERSION > 0
-    .used_gpio_ranges = 0x0
-#endif
-};
-
-static inline pio_sm_config pio_tx_inv_program_get_default_config(uint offset) {
-    pio_sm_config c = pio_get_default_sm_config();
-    sm_config_set_wrap(&c, offset + pio_tx_inv_wrap_target, offset + pio_tx_inv_wrap);
-    sm_config_set_sideset(&c, 2, true, false);
-    return c;
-}
 
 static inline void pio_tx_program_init(PIO pio, uint sm, uint offset, uint pin_tx) {
     // Tell PIO to initially drive output-high on the selected pin, then map PIO
@@ -138,44 +100,6 @@ static const struct pio_program pio_rx_program = {
 static inline pio_sm_config pio_rx_program_get_default_config(uint offset) {
     pio_sm_config c = pio_get_default_sm_config();
     sm_config_set_wrap(&c, offset + pio_rx_wrap_target, offset + pio_rx_wrap);
-    return c;
-}
-#endif
-
-// ---------- //
-// pio_rx_inv //
-// ---------- //
-
-#define pio_rx_inv_wrap_target 0
-#define pio_rx_inv_wrap 6
-#define pio_rx_inv_pio_version 0
-
-static const uint16_t pio_rx_inv_program_instructions[] = {
-    //     .wrap_target
-    0xe032, //  0: set    x, 18
-    0x20a0, //  1: wait   1 pin, 0
-    0xa047, //  2: mov    y, osr
-    0x0083, //  3: jmp    y--, 3
-    0x4001, //  4: in     pins, 1
-    0x0042, //  5: jmp    x--, 2
-    0x8020, //  6: push   block
-    //     .wrap
-};
-
-#if !PICO_NO_HARDWARE
-static const struct pio_program pio_rx_inv_program = {
-    .instructions = pio_rx_inv_program_instructions,
-    .length = 7,
-    .origin = -1,
-    .pio_version = 0,
-#if PICO_PIO_VERSION > 0
-    .used_gpio_ranges = 0x0
-#endif
-};
-
-static inline pio_sm_config pio_rx_inv_program_get_default_config(uint offset) {
-    pio_sm_config c = pio_get_default_sm_config();
-    sm_config_set_wrap(&c, offset + pio_rx_inv_wrap_target, offset + pio_rx_inv_wrap);
     return c;
 }
 
