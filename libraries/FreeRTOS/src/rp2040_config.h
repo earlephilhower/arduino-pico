f5751141eb4d7cd35e0f2293e58e0fa48730d753
Earle F. Philhower, III <earlephilhower@yahoo.com>
2024-09-23 17:54:43 -0700

    Make FreeRTOS-RISCV compile

--- libraries/FreeRTOS/src/rp2040_config.h
+++ libraries/FreeRTOS/src/rp2040_config.h
@@ -1,5 +1,9 @@
 #ifdef PICO_RP2350
+#ifndef __riscv
 #include "../lib/FreeRTOS-Kernel/portable/ThirdParty/GCC/RP2350_ARM_NTZ/non_secure//rp2040_config.h"
+#else
+#include "../lib/FreeRTOS-Kernel/portable/ThirdParty/GCC/RP2350_RISC-V/include/rp2040_config.h"
+#endif
 #else
 #include "../lib/FreeRTOS-Kernel/portable/ThirdParty/GCC/RP2040/include/rp2040_config.h"
 #endif
