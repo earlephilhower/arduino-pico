bfa085adb3962da5b4e0e00425ff6dce1cc78d2a
Earle F. Philhower, III <earlephilhower@yahoo.com>
2024-09-26 11:32:45 -0700

    Punt on GCC14ARM-only new warnings

--- tests/common.sh
+++ tests/common.sh
@@ -234,7 +234,7 @@ function install_ide()
     fi
     # Set custom warnings for all builds (i.e. could add -Wextra at some point)
     echo "compiler.c.extra_flags=-Wall -Wextra -Werror -Wno-ignored-qualifiers $debug_flags" > rp2040/platform.local.txt
-    echo "compiler.cpp.extra_flags=-Wall -Wextra -Werror -Wno-ignored-qualifiers $debug_flags" >> rp2040/platform.local.txt
+    echo "compiler.cpp.extra_flags=-Wall -Wextra -Werror -Wno-ignored-qualifiers -Wno-overloaded-virtual $debug_flags" >> rp2040/platform.local.txt
     echo -e "\n----platform.local.txt----"
     cat rp2040/platform.local.txt
     echo -e "\n----\n"
