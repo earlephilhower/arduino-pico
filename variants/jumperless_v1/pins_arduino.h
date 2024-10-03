2736273aacda62d577bb2ddcb6080045d8b98344
Kevin Santo Cappuccio <KevinC@ppucc.io>
2024-10-03 09:20:37 -0700

    Chmodded makeboards

--- variants/jumperless_v1/pins_arduino.h
+++ variants/jumperless_v1/pins_arduino.h
@@ -10,11 +10,11 @@
 #define PIN_SERIAL1_TX (16u)
 #define PIN_SERIAL1_RX (17u)
 
-#define PIN_SERIAL2_TX (18u)
+#define PIN_SERIAL2_TX (18u) //generally unused
 #define PIN_SERIAL2_RX (19u)
 
 // SPI
-#define PIN_SPI0_MISO  (0u)
+#define PIN_SPI0_MISO  (0u) 
 #define PIN_SPI0_MOSI  (3u)
 #define PIN_SPI0_SCK   (2u)
 #define PIN_SPI0_SS    (1u)
@@ -23,7 +23,7 @@
 #define PIN_WIRE0_SDA  (4u)
 #define PIN_WIRE0_SCL  (5u)
 
-#define PIN_WIRE1_SDA  (18u)
+#define PIN_WIRE1_SDA  (18u) //generally unused
 #define PIN_WIRE1_SCL  (19u)
 
 #define SERIAL_HOWMANY (2u)
