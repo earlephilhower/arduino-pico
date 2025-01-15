b4001bfb0ec77bd580b443b82c1a360e68db674e
Dryw Wade <dryw.wade@sparkfun.com>
2025-01-15 10:47:55 -0700

    Add SparkFun IoT Node LoRaWAN (#2745)
    * added files to support the SparkFun IoT Node LoRaWAN board
    
    * added rp2053 to our lorawan board defs
    
    * adding updates/new files generated for the iot node board
    
    * Add SPI swap
    
    * Remove incorrect comment from IoT Node LoRawAN
    
    * Replace missing line in boards.txt
    
    * Re-run makeboards.py for IoT Node LoRaWAN
    
    ---------
    
    Co-authored-by: Kirk Benell <kirk.benell@sparkfun.com>

--- tools/makeboards.py
+++ tools/makeboards.py
@@ -651,6 +651,7 @@ MakeBoard("sparkfun_promicrorp2040", "rp2040", "SparkFun", "ProMicro RP2040", "0
 MakeBoard("sparkfun_promicrorp2350", "rp2350", "SparkFun", "ProMicro RP2350", "0x1b4f", "0x0026", 250, "SPARKFUN_PROMICRO_RP2350", 16, 8, "none")
 MakeBoard("sparkfun_thingplusrp2040", "rp2040", "SparkFun", "Thing Plus RP2040", "0x1b4f", "0x0026", 250, "SPARKFUN_THINGPLUS_RP2040", 16, 0, "boot2_w25q080_2_padded_checksum")
 MakeBoard("sparkfun_thingplusrp2350", "rp2350", "SparkFun", "Thing Plus RP2350", "0x1b4f", "0x0038", 250, "SPARKFUN_THINGPLUS_RP2350", 16, 8, "none", ["PICO_CYW43_SUPPORTED=1", "CYW43_PIN_WL_DYNAMIC=1"])
+MakeBoard("sparkfun_iotnode_lorawanrp2350", "rp2350", "SparkFun", "IoT Node LoRaWAN", "0x1b4f", "0x0044", 250, "SPARKFUN_IOTNODE_LORAWAN_RP2350", 16, 8, "none")
 
 # Seeed
 MakeBoard("seeed_indicator_rp2040", "rp2040", "Seeed", "INDICATOR RP2040", "0x2886", "0x0050", 250, "SEEED_INDICATOR_RP2040", 2, 0, "boot2_w25q080_2_padded_checksum")
