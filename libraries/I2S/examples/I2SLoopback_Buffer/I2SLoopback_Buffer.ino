4a4a21624706478163ab6a91d33db87b8e39a514
dcooperdalrymple <dcooperdalrymple@gmail.com>
2025-01-24 09:25:22 -0600

    Update type of words to unsigned in example.

--- libraries/I2S/examples/I2SLoopback_Buffer/I2SLoopback_Buffer.ino
+++ libraries/I2S/examples/I2SLoopback_Buffer/I2SLoopback_Buffer.ino
@@ -18,12 +18,12 @@ void setup() {
   i2s.setBCLK(2); // Note: LRCLK = BCLK + 1
   i2s.setBitsPerSample(16);
   i2s.setFrequency(22050);
-  i2s.setBuffers(6, SIZE * sizeof(int16_t) / sizeof(int32_t));
+  i2s.setBuffers(6, SIZE * sizeof(int16_t) / sizeof(uint32_t));
   i2s.begin();
 
   size_t count, index;
   while (1) {
-    count = i2s.read((const uint8_t *)&buffer, SIZE * sizeof(int16_t)) * sizeof(int32_t) / sizeof(int16_t);
+    count = i2s.read((const uint8_t *)&buffer, SIZE * sizeof(int16_t)) * sizeof(uint32_t) / sizeof(int16_t);
     index = 0;
     while (index < count) {
         buffer[index++]; // left
