3846e6fd0fa1011e749d9450d520a6614514a506
dcooperdalrymple <dcooperdalrymple@gmail.com>
2025-01-24 10:12:25 -0600

    Fix formatting of example.

--- libraries/I2S/examples/I2SLoopback_Buffer/I2SLoopback_Buffer.ino
+++ libraries/I2S/examples/I2SLoopback_Buffer/I2SLoopback_Buffer.ino
@@ -26,8 +26,8 @@ void setup() {
     count = i2s.read((const uint8_t *)&buffer, SIZE * sizeof(int16_t)) * sizeof(uint32_t) / sizeof(int16_t);
     index = 0;
     while (index < count) {
-        buffer[index++]; // left
-        buffer[index++]; // right
+      buffer[index++]; // left
+      buffer[index++]; // right
     }
     i2s.write((const uint8_t *)&buffer, count * sizeof(int16_t));
   }
