028fc1f3e99816b460b30d27c9847c053aec6d72
Linus <linus@libewa.xyz>
2025-08-09 12:10:11 +0200

    pimoroni-explorer: Fill in remaining values.

--- variants/pimoroni_explorer/pins_arduino.h
+++ variants/pimoroni_explorer/pins_arduino.h
@@ -3,31 +3,19 @@
 #include <stdint.h>
 
 // Pin definitions taken from:
-//    https://github.com/pimoroni/explorer/blob/main/explorer/explorer.h
+//    https://github.com/pimoroni/explorer/blob/main/explorer/
 //    https://github.com/pimoroni/explorer/blob/main/examples/lib/explorer.py
 //    https://cdn.shopify.com/s/files/1/0174/1800/files/Pimoroni_Explorer_Schematic.pdf?v=1729171995
 
+#define PICO_RP2350A 0
 
-//TODO: Is this the chip or board? Do the buttons count?
-#define PINS_COUNT          (33u)
-#define NUM_DIGITAL_PINS    (33u)
+//TODO: Do the buttons count?
+#define NUM_DIGITAL_PINS    (12u)
 #define NUM_ANALOG_INPUTS   (6u)
 #define NUM_ANALOG_OUTPUTS  (0u)
 #define ADC_RESOLUTION      (12u)
 
-// Serial/UART?
-#define PIN_SERIAL1_TX      (0u)
-#define PIN_SERIAL1_RX      (1u)
-
-//TODO: SPI? Does the QSPI for flash count?
-/*
-#define PIN_SPI0_MISO       (32u)
-#define PIN_SPI0_MOSI       (35u)
-#define PIN_SPI0_SCK        (34u)
-#define PIN_SPI0_SS         (33u)
-*/
-
-#define SERIAL_HOWMANY      (3u)
+#define SERIAL_HOWMANY      (0u)
 #define SPI_HOWMANY         (0u)
 #define WIRE_HOWMANY        (1u)
 
@@ -41,7 +29,7 @@ static const uint8_t GP3 = (3u);
 static const uint8_t GP4 = (4u);
 static const uint8_t GP5 = (5u);
 
-//TODO: Servos
+//Servos
 #define PIN_SERVO1          (6u)
 #define PIN_SERVO2          (7u)
 #define PIN_SERVO3          (8u)
@@ -54,7 +42,6 @@ static const uint8_t GP5 = (5u);
 #define PIN_SPEAKER_AMP_ENABLE (13u)
 
 // on-board buttons
-//TODO: Should the switches be automatically set to INPUT_PULLUP?
 static const uint8_t SWITCH_C = (14u);
 static const uint8_t SWITCH_B = (15u);
 static const uint8_t SWITCH_A = (16u);
@@ -67,14 +54,13 @@ static const uint8_t SWITCH_Z = (19u);
 #define PIN_WIRE0_SCL       (21u)
 
 // Boot button, also referred to as the user switch
+//TODO: Call it SWITCH_BOOT or SWITCH_USER?
 static const uint8_t SWITCH_BOOT = (22u);
 
 // Pin 23 is NC
 // Pin 24 is NC
 // Pin 25 is NC
-
-//TODO: Define LCD_BL as LED_BUILTIN? it would look awful, but ensure compatibility.
-// Using Pin 25 as LED_BUILTIN also works, but the user might wonder why nothing's happening.
+#define PIN_LED 25 // The LED will not blink, but sketches will compile
 
 //TODO: LCD
 #define PIN_LCD_BL          (26u) 
@@ -98,3 +84,30 @@ static const uint8_t ADC2 = (42u);
 static const uint8_t ADC3 = (43u);
 static const uint8_t ADC4 = (44u);
 static const uint8_t ADC5 = (45u);
+
+#define LED_BUILTIN PIN_LED
+
+/* Definitions to make people happy (these don't actually exist) 
+I'm setting them to GPIO10, which is NC. */
+
+// Serial
+#define PIN_SERIAL1_TX (10u)
+#define PIN_SERIAL1_RX (10u)
+
+#define PIN_SERIAL2_TX (10u)
+#define PIN_SERIAL2_RX (10u)
+
+// SPI
+#define PIN_SPI0_MISO  (10u)
+#define PIN_SPI0_MOSI  (10u)
+#define PIN_SPI0_SCK   (10u)
+#define PIN_SPI0_SS    (10u)
+
+#define PIN_SPI1_MISO  (10u)
+#define PIN_SPI1_MOSI  (10u)
+#define PIN_SPI1_SCK   (10u)
+#define PIN_SPI1_SS    (10u)
+
+// Wire1
+#define PIN_WIRE1_SDA  (10u)
+#define PIN_WIRE1_SCL  (10u)
