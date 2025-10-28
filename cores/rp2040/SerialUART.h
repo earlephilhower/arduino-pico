03cc9901546d579d6d30600812095fde2271a54c
Nat Gist <25966140+ngist@users.noreply.github.com>
2025-10-28 01:58:56 +0000

    [UART] Adds SerialUART::getActualBaud() (#3225)

--- cores/rp2040/SerialUART.h
+++ cores/rp2040/SerialUART.h
@@ -93,6 +93,11 @@ public:
     // on read)
     bool getBreakReceived();
 
+    // Returns the baud rate the uart is actually operating at vs the desired baud rate specified to begin()
+    int getActualBaud() {
+        return _running ? _actualBaud : 0;
+    }
+
 private:
     bool _running = false;
     uart_inst_t *_uart;
@@ -100,6 +105,7 @@ private:
     pin_size_t _rts, _cts;
     gpio_function_t _fcnTx, _fcnRx, _fcnRts, _fcnCts;
     int _baud;
+    int _actualBaud;
     mutex_t _mutex;
     bool _polling = false;
     bool _overflow;
