cf226fef22aba5c367d43cb3cb082a7ffed53255
Earle F. Philhower, III <earlephilhower@yahoo.com>
2025-11-21 09:44:25 -0800

    Add I2S input slave support (#3244)
    Due to the way the PIO inputs work, slave input mode needs to have
    its pins defined in a fixed order:  DIN, BCLK, LRCLK
    
    For slave mode, specify ``setDATA`` . ``setBCLK`` and ``setLRCLK``
    are ignored.

--- libraries/I2S/src/pio_i2s.pio.h
+++ libraries/I2S/src/pio_i2s.pio.h
@@ -324,6 +324,49 @@ static inline pio_sm_config pio_i2s_in_program_get_default_config(uint offset) {
 }
 #endif
 
+// ---------------- //
+// pio_i2s_in_slave //
+// ---------------- //
+
+#define pio_i2s_in_slave_wrap_target 3
+#define pio_i2s_in_slave_wrap 11
+#define pio_i2s_in_slave_pio_version 0
+
+static const uint16_t pio_i2s_in_slave_program_instructions[] = {
+    0x2021, //  0: wait   0 pin, 1
+    0x20a1, //  1: wait   1 pin, 1
+    0x00c0, //  2: jmp    pin, 0
+    //     .wrap_target
+    0x2021, //  3: wait   0 pin, 1
+    0x20a1, //  4: wait   1 pin, 1
+    0x4001, //  5: in     pins, 1
+    0x00c8, //  6: jmp    pin, 8
+    0x0003, //  7: jmp    3
+    0x2021, //  8: wait   0 pin, 1
+    0x20a1, //  9: wait   1 pin, 1
+    0x4001, // 10: in     pins, 1
+    0x00c8, // 11: jmp    pin, 8
+    //     .wrap
+};
+
+#if !PICO_NO_HARDWARE
+static const struct pio_program pio_i2s_in_slave_program = {
+    .instructions = pio_i2s_in_slave_program_instructions,
+    .length = 12,
+    .origin = -1,
+    .pio_version = pio_i2s_in_slave_pio_version,
+#if PICO_PIO_VERSION > 0
+    .used_gpio_ranges = 0x0
+#endif
+};
+
+static inline pio_sm_config pio_i2s_in_slave_program_get_default_config(uint offset) {
+    pio_sm_config c = pio_get_default_sm_config();
+    sm_config_set_wrap(&c, offset + pio_i2s_in_slave_wrap_target, offset + pio_i2s_in_slave_wrap);
+    return c;
+}
+#endif
+
 // ------------- //
 // pio_i2s_inout //
 // ------------- //
@@ -510,6 +553,21 @@ static inline void pio_i2s_in_program_init I2S_INIT_PARAMS {
     pio_sm_exec(pio, sm, pio_encode_set(pio_y, bits - 2));
     pio_sm_exec(pio, sm, pio_encode_in(pio_pins, bits)); // Shift in 1st L data
     pio_sm_exec(pio, sm, pio_encode_in(pio_pins, bits - 1)); // Shift in 1st R data modulo one bit, avoiding bit shift from #2037
+}
+static inline void pio_i2s_in_slave_program_init I2S_INIT_PARAMS {
+    I2S_INIT_PARAMS_VOID;
+    // Note that the CLOCK_BASE pin is ignored.  It's DIN, BCLK, LRCLK consecutive due to PIO limitations
+    pio_gpio_init(pio, data_in_pin);
+    pio_gpio_init(pio, data_in_pin + 1);
+    pio_gpio_init(pio, data_in_pin + 2);
+    pio_sm_config sm_config = pio_i2s_in_slave_program_get_default_config(offset);
+    sm_config_set_in_pins(&sm_config, data_in_pin);
+    sm_config_set_in_pin_count(&sm_config, 3);
+    sm_config_set_in_shift(&sm_config, false, true, (bits <= 16) ? 2 * bits : bits);
+    sm_config_set_fifo_join(&sm_config, PIO_FIFO_JOIN_RX);
+    sm_config_set_jmp_pin(&sm_config, data_in_pin + 2);
+    pio_sm_init(pio, sm, offset, &sm_config);
+    pio_sm_set_consecutive_pindirs(pio, sm, data_in_pin, 3, false);
 }
 static inline void pio_i2s_inout_program_init I2S_INIT_PARAMS {
     I2S_INIT_PARAMS_VOID;
