30aead9a6c478ff9951c8be708e68be92808cdab
Earle F. Philhower, III <earlephilhower@yahoo.com>
2025-12-09 10:17:40 -0800

    Separate USB gamepad16 report and descriptor (#3263)
    Fixes #3262
    
    BTStack redefines many USB HID definitions, so need to be careful to not include
    full tusb.h before btstack.h.  Keyboard and Mouse didn't need special report
    structs, but Joystick (joystick16) did, so it was including TUSB and BTstack
    and barfing.
    
    Separate the joystick report struct definition from the HID descriptor
    to avoid the issue.

---
+++ cores/rp2040/sdkoverride/tusb_gamepad16_report.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdint.h>
+
+// HID Gamepad Protocol Report.
+typedef struct TU_ATTR_PACKED {
+    int16_t x;         ///< Delta x  movement of left analog-stick
+    int16_t y;         ///< Delta y  movement of left analog-stick
+    int16_t z;         ///< Delta z  movement of right analog-joystick
+    int16_t rz;        ///< Delta Rz movement of right analog-joystick
+    int16_t rx;        ///< Delta Rx movement of analog left trigger
+    int16_t ry;        ///< Delta Ry movement of analog right trigger
+    uint8_t hat;       ///< Buttons mask for currently pressed buttons in the DPad/hat
+    uint32_t buttons;  ///< Buttons mask for currently pressed buttons
+} hid_gamepad16_report_t;
