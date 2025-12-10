ae142c9d2f0af1c2fa396753a9291e048a87d968
Earle F. Philhower, III <earlephilhower@yahoo.com>
2025-12-10 14:55:37 -0800

    Astyle

--- cores/rp2040/sdkoverride/ncm_device.c
+++ cores/rp2040/sdkoverride/ncm_device.c
@@ -17,11 +17,14 @@
 
 extern bool tud_network_can_xmit(uint16_t size) __attribute((weak));
 bool tud_network_can_xmit(uint16_t size) {
+    (void) size;
     return false;
 }
 
 extern void tud_network_xmit(void *ref, uint16_t arg) __attribute((weak));
 void tud_network_xmit(void *ref, uint16_t arg) {
+    (void) ref;
+    (void) arg;
     return;
 }
 
@@ -35,26 +38,36 @@ void netd_init(void) {
 
 extern bool netd_deinit(void) __attribute((weak));
 bool netd_deinit(void) {
-  return true;
+    return true;
 }
 
 extern void netd_reset(uint8_t rhport) __attribute((weak));
 void netd_reset(uint8_t rhport) {
-  (void) rhport;
+    (void) rhport;
 }
 
 extern uint16_t netd_open(uint8_t rhport, tusb_desc_interface_t const *itf_desc, uint16_t max_len) __attribute((weak));
 uint16_t netd_open(uint8_t rhport, tusb_desc_interface_t const *itf_desc, uint16_t max_len) {
+    (void) rhport;
+    (void) itf_desc;
+    (void) max_len;
     return 0;
 }
 
 extern bool netd_xfer_cb(uint8_t rhport, uint8_t ep_addr, xfer_result_t result, uint32_t xferred_bytes) __attribute((weak));
 bool netd_xfer_cb(uint8_t rhport, uint8_t ep_addr, xfer_result_t result, uint32_t xferred_bytes) {
+    (void) rhport;
+    (void) ep_addr;
+    (void) result;
+    (void) xferred_bytes;
     return false;
 }
 
 extern bool netd_control_xfer_cb(uint8_t rhport, uint8_t stage, tusb_control_request_t const *request) __attribute((weak));
 bool netd_control_xfer_cb(uint8_t rhport, uint8_t stage, tusb_control_request_t const *request) {
+    (void) rhport;
+    (void) stage;
+    (void) request;
     return false;
 }
 
