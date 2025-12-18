22922b3adcf161e62516cffd9c9497f3b1fca64a
Earle F. Philhower, III <earlephilhower@yahoo.com>
2025-12-17 18:20:02 -0800

    One CB is never used in HID, give dummy implement

--- libraries/tusb-hid/src/hid_device.c
+++ libraries/tusb-hid/src/hid_device.c
@@ -1,1 +1,15 @@
 #include "../../../pico-sdk/lib/tinyusb/src/class/hid/hid_device.c"
+
+// Invoked when received GET_REPORT control request
+// Application must fill buffer report's content and return its length.
+// Return zero will cause the stack to STALL request
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen) {
+    // TODO not implemented
+    (void) instance;
+    (void) report_id;
+    (void) report_type;
+    (void) buffer;
+    (void) reqlen;
+
+    return 0;
+}
