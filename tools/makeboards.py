38efc51d52f6ec10df7976a413cc747bab2eb324
Abhi-00047 <an5600580@gmail.com>
2026-06-27 07:16:37 +0000

    Fix display names for PCBCupid Glyph 2040 and Glyph Mini 2040

--- tools/makeboards.py
+++ tools/makeboards.py
@@ -673,10 +673,10 @@ MakeBoard("pimoroni_explorer", "rp2350", "Pimoroni", "Explorer", "0x2e8a", "0x10
 MakeBoard("pintronix_pinmax", "rp2040", "Pintronix", "PinMax", "0x2e8a", "0x9101", 250, "PINTRONIX_PINMAX", 4, 0, "boot2_w25q080_2_padded_checksum")
 
 # PCBCupid
-MakeBoard("pcbcupid_glyph_2040", "rp2040", "PCBCupid", "Glyph 2040", "0x2e8a", "0x1128", 250, "PCBCUPID_GLYPH_2040", 8, 0, "boot2_w25q64jv_4_padded_checksum", None, "https://pcbcupid.com")
+MakeBoard("pcbcupid_glyph_2040", "rp2040", "PCBCupid", "Pcbcupid GLYPH 2040", "0x2e8a", "0x1128", 250, "PCBCUPID_GLYPH_2040", 8, 0, "boot2_w25q64jv_4_padded_checksum", None, "https://pcbcupid.com")
 
 # PCBCupid
-MakeBoard("pcbcupid_glyph_mini_2040", "rp2040", "PCBCupid", "Glyph Mini 2040", "0x2e8a", "0x1123", 250, "PCBCUPID_GLYPH_MINI_2040", 8, 0, "boot2_w25q64jv_4_padded_checksum", None, "https://pcbcupid.com")
+MakeBoard("pcbcupid_glyph_mini_2040", "rp2040", "PCBCupid", "Pcbcupid GLYPH MINI 2040", "0x2e8a", "0x1123", 250, "PCBCUPID_GLYPH_MINI_2040", 8, 0, "boot2_w25q64jv_4_padded_checksum", None, "https://pcbcupid.com")
 
 # RAKwireless
 MakeBoard("rakwireless_rak11300", "rp2040", "RAKwireless", "RAK11300", "0x2e8a", "0x00c0", 500, "RAKWIRELESS_RAK11300", 2, 0, "boot2_w25q16jvxq_4_padded_checksum", None, "https://store.rakwireless.com/products/wisduo-lpwan-module-rak11300")
